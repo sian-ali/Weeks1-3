4f107a08f01517e89d3e2d84c8f8cb30fe300578
Sian Ali <alisia@shernet.sheridancollege.ca>
2026-01-06 11:28:31 -0500

    made thingy magic move f

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    float speed = 0.01f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = transform.position;
+        newPos.x += 0.1f;
+        transform.position = newPos;
+        if (transform.position.x < 0 || transform.position.x > 8)
+    }
+}
