99f5a9df9596328516ac749abfe5115dfd5695d8
Sian Ali <alisia@shernet.sheridancollege.ca>
2026-01-20 11:44:40 -0500

    clock SPEED

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public float speed = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z += speed * Time.deltaTime;
+        transform.eulerAngles = newRotation;
+
+        
+    }
+}
