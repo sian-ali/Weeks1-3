4313990a2345498bc1df1fb3c603e6ae74d6fe26
alisia <alisia@OA-S141-22>
2026-05-12 17:36:45 -0400

    in class excercises week 2 bouncing ball, timer, guard etc.

--- Assets/Scripts/Bouncing Ball.cs
+++ Assets/Scripts/Bouncing Ball.cs
@@ -1,44 +1,46 @@
 using UnityEngine;
 
-public class SquareMover : MonoBehaviour
+public class BouncingBall : MonoBehaviour
 {
-    public float speed = 0.01f;
+    public float speed = 5f;
     float xMax = 8.3f;
     float xMin = -8.3f;
-
+    float yMax = 4.5f;
+    float yMin = -4.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //we can get access to the transform component by typing "transform"
-
-        //on the transform we can access the position which is a Vector3
-        //Vector3 contains x y and z values grouped together
-        //transform.position.x += 1f;
-
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //turn this object a random colour
+       
         Vector3 newPosition = transform.position;
 
-        //when assigning a value to float, we want to append "f" to the end
+        
         newPosition.x += speed * Time.deltaTime;
+        newPosition.y += speed * Time.deltaTime;
         transform.position = newPosition;
 
         if (newPosition.x > xMax)
         {
             speed = speed * -1f;
         }
-        //also speed *= -1f;
-        //also also speed = -speed;
         if (newPosition.x < xMin)
         {
             speed = speed * -1f;
         }
-     
+        if (newPosition.y > yMax)
+        {  
+            speed = speed * -1f; 
+        }
+        if (newPosition.y < yMin)
+        {
+            speed = speed * -1f;
+        }
+
     }
 }
