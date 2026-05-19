4a1f5ac626e26074f052b63c3932e657eda39146
alisia <alisia@OA-S141-22>
2026-05-19 17:54:08 -0400

    started making moon move

---
+++ Assets/Scripts/Moon.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    public float speed = 0.1f;
+    float xMax = 9.5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x += speed * Time.deltaTime;
+        transform.position = newPosition;
+
+        if (newPosition.x > xMax)
+        {
+            speed = 0f;
+        }
+       
+    }
+}
