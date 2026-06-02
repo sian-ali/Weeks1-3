af65867e565446607a45d97ecacffb8270eac9f4
Sian Ali <alisia@shernet.sheridancollege.ca>
2026-06-01 23:34:57 -0400

    fire and hair move

--- Assets/Scripts/Hair.cs
+++ Assets/Scripts/Hair.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Hair : MonoBehaviour
 {
+    public float speed = 0.1f;
+    public float xMax;
+    public float xMin;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPosition = transform.position;
+        newPosition.x += speed * Time.deltaTime;
+        transform.position = newPosition;
+
+        if (newPosition.x > xMax)
+        {
+            speed = speed * -1f;
+        }
+        if (newPosition.x < xMin)
+        {
+            speed = speed * -1f;
+        }
     }
 }
